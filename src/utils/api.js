a528c919e0c7872dd8bd8fe8bdbf938777726339
Amansingh-afk <amansinghafk@gmail.com>
2023-09-09 22:41:22 +0530

    prod fix.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/v1", // http://localhost:8000/api/v1/ ...
+  baseURL: "https://clubhub-backend.onrender.com/api/v1/", // http://localhost:8000/api/v1/ ...
   headers: {
     "Content-Type": "application/json",
   },
